94e2cbe0b56846280346667bfb4b3d406fd5d4e8
achmadss <anakinskywalk1@gmail.com>
2022-10-19 11:47:29 +0700

    test: BaseScreen
    - test BaseScreen UI

--- app/src/main/java/app/trian/socialapp/MainActivity.kt
+++ app/src/main/java/app/trian/socialapp/MainActivity.kt
@@ -3,35 +3,66 @@ package app.trian.socialapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
-import com.cexup.ui.consumer.screen.ScreenOnBoarding
-import com.cexup.ui.consumer.screen.auth.ScreenSignUp
-import com.cexup.ui.consumer.screen.clinic_reservation.ClinicHospitalUIState
-import com.cexup.ui.consumer.screen.clinic_reservation.ScreenChooseScheduleClinicReservation
-import com.cexup.ui.consumer.screen.clinic_reservation.ScreenListClinicReservation
-import com.cexup.ui.consumer.theme.ConsumerTheme
+import androidx.compose.material.Text
+import com.cexup.ui.corporate.component.SidebarMenuModel
+import com.cexup.ui.corporate.screen.BaseScreen
+import com.cexup.ui.corporate.theme.CorporateTheme
+import com.cexup.ui.R
+import com.cexup.ui.corporate.component.SidebarMenuType
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ConsumerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+            CorporateTheme {
+                BaseScreen(
+                    currentRoute = "",
+                    onSearchPatient = {  },
+                    onPatientDetail = {  },
+                    onCheckUp = {  },
+                    onAddPatient = {  },
+                    onProfile = {  },
+                    onLogout = {  },
+                    onNavigate = {  },
+                    listMenuSidebar = listOf(
+                        SidebarMenuModel(
+                            name= R.string.corporate_menu_dashboard,
+                            image = R.drawable.ic_home_unselected,
+                            selectedImage = R.drawable.ic_home_selected,
+                            type= SidebarMenuType.Link,
+                            path = ""
+                        ),
+                        SidebarMenuModel(
+                            name= R.string.corporate_menu_patients,
+                            image = R.drawable.ic_patient_unselected ,
+                            selectedImage = R.drawable.ic_patient_selected,
+                            type= SidebarMenuType.Link,
+                            path = ""
+                        ),
+                        SidebarMenuModel(
+                            name= R.string.corporate_menu_cexup,
+                            image = R.drawable.ic_feature_unselected ,
+                            selectedImage = R.drawable.ic_feature_selected,
+                            type= SidebarMenuType.Link,
+                            path = ""
+                        ),
+                        SidebarMenuModel(
+                            name= R.string.corporate_menu_account,
+                            image = R.drawable.ic_user_unselected ,
+                            selectedImage = R.drawable.ic_user_selected,
+                            type= SidebarMenuType.Link,
+                            path = ""
+                        ),
+                        SidebarMenuModel(
+                            name= R.string.corporate_menu_logout,
+                            image = R.drawable.ic_logout_unselected ,
+                            selectedImage = R.drawable.ic_logout_selected,
+                            type= SidebarMenuType.Button,
+                            path = ""
+                        ),
+                    )
                 ) {
-                    ScreenListClinicReservation(
-                        hospitalState = ClinicHospitalUIState(
-                        loading = true,
-                        error = false,
-                        data = listOf()),
-                        onSearchHospital = {})
-
+                    Text(text = "Test")
                 }
             }
         }
