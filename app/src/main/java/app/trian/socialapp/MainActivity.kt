de265ef3679bde4af2dc5f17548706eb633d21b6
achmadss <anakinskywalk1@gmail.com>
2022-10-20 13:17:59 +0700

    test: ScreenCheckup

--- app/src/main/java/app/trian/socialapp/MainActivity.kt
+++ app/src/main/java/app/trian/socialapp/MainActivity.kt
@@ -1,14 +1,16 @@
 package app.trian.socialapp
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.Text
-import com.cexup.ui.corporate.component.SidebarMenuModel
+import androidx.compose.ui.platform.LocalContext
 import com.cexup.ui.corporate.screen.BaseScreen
 import com.cexup.ui.corporate.theme.CorporateTheme
 import com.cexup.ui.R
-import com.cexup.ui.corporate.component.SidebarMenuType
+import com.cexup.ui.corporate.component.*
+import com.cexup.ui.corporate.screen.PhysicalExamination
+import com.cexup.ui.corporate.screen.ScreenCheckup
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -62,7 +64,39 @@ class MainActivity : ComponentActivity() {
                         ),
                     )
                 ) {
-                    Text(text = "Test")
+                    val context = LocalContext.current
+                    val featureList = listOf(
+                        PhysicalExamination(
+                            icon = R.drawable.ic_multiparameter,
+                            nameFeature = "Feature 1",
+                            typePhysicalExamination = TypePhysicalExamination.NoValue,
+                        ) {
+                            Toast.makeText(context, "Feature 1 $it", Toast.LENGTH_SHORT).show()
+                        },
+                        PhysicalExamination(
+                            icon = R.drawable.ic_multiparameter,
+                            nameFeature = "Feature 2",
+                            typePhysicalExamination = TypePhysicalExamination.NoValue,
+                        ) {
+                            Toast.makeText(context, "Feature 2 $it", Toast.LENGTH_SHORT).show()
+                        }
+                    )
+                    val patients = listOf(
+                        Pair(first = "Name 1", second = "UserCode 1"),
+                        Pair(first = "Name 2", second = "UserCode 2"),
+                        Pair(first = "Name 3", second = "UserCode 3"),
+                        Pair(first = "Name 4", second = "UserCode 4"),
+                    )
+                    ScreenCheckup(
+                        featureList = featureList,
+                        onClickSyncToCloud = {
+                            Toast.makeText(context, "Sync to Cloud", Toast.LENGTH_SHORT).show()
+                        },
+                        patients = patients,
+                        onPatientSelected = {
+                            Toast.makeText(context, it, Toast.LENGTH_SHORT).show()
+                        }
+                    )
                 }
             }
         }
